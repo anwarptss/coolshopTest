11a027fbcddcf8769ba930e4757af1d80b0efa94
Anwar Ahmed <anwar777.300@gmail.com>
2023-07-04 23:01:59 +0400

    Update Program.cs

--- coolshopTest/Program.cs
+++ coolshopTest/Program.cs
@@ -1,9 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using Microsoft.VisualBasic.FileIO;
 using System.Data;
 using IronXL;
 using coolshopTest;
 using coolshopTest.DTOs;
+using System.Diagnostics;
 
 Console.WriteLine("\t\t\t------------- Welcome to the CoolShop Test! -----------------\n\n");
 char rep;
@@ -11,8 +12,15 @@ do
 {
     try
     {
-        string fileName = "file.csv";
-        string filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+        string customFileURL = "";
+        string fileName = "File\\file.csv";
+
+        string[] fileURL = Environment.CurrentDirectory.Split('\\');
+        for (int i = 0; i < fileURL.Length - 3; i++)
+        {
+            customFileURL += fileURL[i] + "\\"; 
+        }
+        string filePath = customFileURL + fileName;//Path.Combine(Environment.CurrentDirectory, fileName);
         Common.ReadCSVData(filePath);
     }
     catch (Exception ex)
@@ -26,3 +34,4 @@ do
 
 } while (rep == 'y' || rep == 'Y');
 
+ 
